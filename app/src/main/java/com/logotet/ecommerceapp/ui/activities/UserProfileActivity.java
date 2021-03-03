a887ccaf68212a415f7c6ff3f3583232ddc2703c
logotet <logotetsp@gmail.com>
2021-03-03 11:42:50 +0200

    User Profile Activity Added

--- app/src/main/java/com/logotet/ecommerceapp/ui/activities/UserProfileActivity.java
+++ app/src/main/java/com/logotet/ecommerceapp/ui/activities/UserProfileActivity.java
@@ -1,16 +1,20 @@
 package com.logotet.ecommerceapp.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import com.logotet.ecommerceapp.R;
+import com.logotet.ecommerceapp.databinding.ActivityUserProfileBinding;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private ActivityUserProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_profile);
     }
 }
